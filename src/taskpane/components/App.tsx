1d9d2413acaa9f7ba45f32a7f2674070771fd7cb
Michael Humann <Kingpin0509@gmail.com>
2023-10-05 00:39:50 +0200

    Vers.1.0.8 - PalettenboxVorlagen ProdReady

--- src/taskpane/components/App.tsx
+++ src/taskpane/components/App.tsx
@@ -50,7 +50,7 @@ export default class App extends React.Component<AppProps, AppState> {
 
       //Paragraph0
       // insert a paragraph at the start of the document.
-      const paragraph = context.document.body.insertParagraph("BIO", Word.InsertLocation.start);
+      const paragraph = context.document.body.insertParagraph("BIO", Word.InsertLocation.end);
       // change the paragraph color to Limnette.
       paragraph.font.color = "#BED200";
       // change the paragraph size to 72.
@@ -135,22 +135,105 @@ export default class App extends React.Component<AppProps, AppState> {
     });
   };
 
-  click1 = async () => {
+  clickKapseln = async () => {
+    return Word.run(async (context) => {
+      /**
+       * Insert Word code here
+       */
+
+      //Paragraph0
+      // insert a paragraph at the start of the document.
+      const paragraph = context.document.body.insertParagraph("", Word.InsertLocation.end);
+      // change the paragraph color to Limnette.
+      // paragraph.font.color = "#BED200";
+      // change the paragraph size to 72.
+      paragraph.font.size = 72;
+      // change the paragraph font family to Montserrat.
+      paragraph.font.name = "Montserrat ExtraBold";
+      // change the paragraph text align to center.
+      paragraph.alignment = "Centered";
+
+      //Paragraph1
+      // insert a empty paragraph at the end of the document. size to 48. and center.
+      const paragraph1 = context.document.body.insertParagraph("", Word.InsertLocation.end);
+      paragraph1.font.size = 48;
+      paragraph1.font.name = "Montserrat ExtraBold";
+      paragraph1.alignment = "Centered";
+
+      //Paragraph2
+      // insert a paragraph at the end of the document. change color to Black. size to 48. font to Montserrat. and center.
+      const paragraph2 = context.document.body.insertParagraph("Produktname", Word.InsertLocation.end);
+      paragraph2.font.color = "#000000";
+      paragraph2.font.size = 48;
+      paragraph2.font.name = "Montserrat ExtraBold";
+      paragraph2.alignment = "Centered";
+
+      //Paragraph3
+      // insert a paragraph at the end of the document. change color to Black. size to 48. font to Montserrat. and center.
+      const paragraph3 = context.document.body.insertParagraph("Pulver-Kapseln", Word.InsertLocation.end);
+      paragraph3.font.color = "#000000";
+      paragraph3.font.size = 48;
+      paragraph3.font.name = "Montserrat ExtraBold";
+      paragraph3.alignment = "Centered";
+
+      //Paragraph4
+      // insert a empty paragraph at the end of the document. size to 36. and center.
+      const paragraph4 = context.document.body.insertParagraph("", Word.InsertLocation.end);
+      paragraph4.font.size = 36;
+      paragraph4.font.name = "Montserrat ExtraBold";
+      paragraph4.alignment = "Centered";
+
+      //Paragraph5
+      // insert a empty paragraph at the end of the document. size to 36.
+      // const paragraph5 = context.document.body.insertParagraph("", Word.InsertLocation.end);
+      // paragraph5.font.size = 36;
+
+      //Paragraph6
+      // insert a paragraph at the end of the document. change color to Black. size to 48. font to Montserrat. and center.
+      const paragraph6 = context.document.body.insertParagraph("Kundenname", Word.InsertLocation.end);
+      paragraph6.font.color = "#000000";
+      paragraph6.font.size = 48;
+      paragraph6.font.name = "Montserrat ExtraBold";
+      paragraph6.alignment = "Centered";
+
+      //Paragraph7
+      // insert a empty paragraph at the end of the document. size to 48. and center.
+      const paragraph7 = context.document.body.insertParagraph("", Word.InsertLocation.end);
+      paragraph7.font.size = 36;
+      paragraph7.font.name = "Montserrat ExtraBold";
+      paragraph7.alignment = "Centered";
+
+      //Paragraph8
+      // insert a empty paragraph at the end of the document. size to 48.
+      // const paragraph8 = context.document.body.insertParagraph("", Word.InsertLocation.end);
+      // paragraph8.font.size = 48;
+
+      //Paragraph9
+      // insert a paragraph at the end of the document. change color to Black. size to 72. font to Montserrat. and center.
+      const paragraph9 = context.document.body.insertParagraph("AFK-000", Word.InsertLocation.end);
+      paragraph9.font.color = "#000000";
+      paragraph9.font.size = 72;
+      paragraph9.font.name = "Montserrat ExtraBold";
+      paragraph9.alignment = "Centered";
+
+      //Paragraph10
+      // insert a paragraph at the end of the document. change color to Black. size to 72. font to Montserrat. and center.
+      const paragraph10 = context.document.body.insertParagraph("000-A", Word.InsertLocation.end);
+      paragraph10.font.color = "#000000";
+      paragraph10.font.size = 72;
+      paragraph10.font.name = "Montserrat ExtraBold";
+      paragraph10.alignment = "Centered";
+      await context.sync();
+    });
+  };
+  click = async () => {
     return Word.run(async (context) => {
       /**
        * Insert your Word code here
        */
-
-      // insert a paragraph at the end of the document.
-      const paragraph = context.document.body.insertParagraph("Kapselsäcke - Bio", Word.InsertLocation.end);
-
-      // change the paragraph color to green.
-      paragraph.font.color = "green";
-
       await context.sync();
     });
   };
-
   render() {
     const { title, isOfficeInitialized } = this.props;
 
@@ -183,7 +266,7 @@ export default class App extends React.Component<AppProps, AppState> {
             <DefaultButton
               className="ms-welcome__action"
               iconProps={{ iconName: "ChevronRight" }}
-              onClick={this.clickBioKapseln}
+              onClick={this.clickKapseln}
             >
               Kapselsäcke
             </DefaultButton>
@@ -197,49 +280,25 @@ export default class App extends React.Component<AppProps, AppState> {
           </span>
           <h2>Produktion - Kartons</h2>
           <span className="ms-template-list">
-            <DefaultButton
-              className="ms-welcome__action"
-              iconProps={{ iconName: "ChevronRight" }}
-              onClick={this.click1}
-            >
+            <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronRight" }} onClick={this.click}>
               Kapseldosen ohne Etikett
             </DefaultButton>
-            <DefaultButton
-              className="ms-welcome__action"
-              iconProps={{ iconName: "ChevronRight" }}
-              onClick={this.click1}
-            >
+            <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronRight" }} onClick={this.click}>
               Kapseldosen ohne Etikett - Bio
             </DefaultButton>
-            <DefaultButton
-              className="ms-welcome__action"
-              iconProps={{ iconName: "ChevronRight" }}
-              onClick={this.click1}
-            >
+            <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronRight" }} onClick={this.click}>
               Dosen bedruckt
             </DefaultButton>
-            <DefaultButton
-              className="ms-welcome__action"
-              iconProps={{ iconName: "ChevronRight" }}
-              onClick={this.click1}
-            >
+            <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronRight" }} onClick={this.click}>
               Dosen bedruckt - Bio
             </DefaultButton>
           </span>
           <h2>Lager - Hochregal</h2>
           <span className="ms-template-list">
-            <DefaultButton
-              className="ms-welcome__action"
-              iconProps={{ iconName: "ChevronRight" }}
-              onClick={this.click1}
-            >
+            <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronRight" }} onClick={this.click}>
               Kapseldosen
             </DefaultButton>
-            <DefaultButton
-              className="ms-welcome__action"
-              iconProps={{ iconName: "ChevronRight" }}
-              onClick={this.click1}
-            >
+            <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronRight" }} onClick={this.click}>
               Kapseldosen - Bio
             </DefaultButton>
           </span>
